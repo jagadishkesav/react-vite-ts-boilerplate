d50e00372a60704b4de6efda303825670fae2ca2
Jagadish <jagadishkesav@users.noreply.github.com>
2025-03-16 13:54:49 +0530

    Fix import error in App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { AppRoutes } from '@/routes';
+import { AppRoutes } from '@/router';
 import { theme } from '@/styles/theme';
 
 // Initialize the QueryClient
